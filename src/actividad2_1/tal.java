143945c3322860d42c0383e61dfbd9af436a2c3f
Sergio <Sergio@DESKTOP-O35L3I8>
2022-11-10 11:46:59 +0100

    tal y tal

---
+++ src/actividad2_1/tal.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package actividad2_1;
+
+/**
+ *
+ * @author Sergio
+ */
+public class tal {
+    public static int raiz(int a,int b, int c){
+        return Math.sqrt(a);
+    }
+}
